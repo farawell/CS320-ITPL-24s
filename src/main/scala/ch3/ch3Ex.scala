9789ab1c93d67dd282490f304aab990f3d2d89dd
John Park <john7park@naver.com>
2024-04-08 03:09:23 +0900

    Implemented the exercises of Ch3

--- src/main/scala/ch3/ch3Ex.scala
+++ src/main/scala/ch3/ch3Ex.scala
@@ -1,5 +1,32 @@
 package ch3
+import scala.annotation.tailrec
 
 object ch3Exercises{
-    
+    case class Student(name: String, height: Int)
+    // Exercise 3.1
+    def names(l: List[Student]): List[String] = l match {
+        case Nil => Nil
+        case h :: t => h.name :: names(t)
+    }
+
+    // Exercise 3.2
+    def tall(l: List[Student]): List[Student] = {
+        l.filter(_.height >= 170)
+    }
+
+    // Exercise 3.3
+    def length(l: List[Int]): Int = {
+        var res = 0
+        @tailrec def lenCal(l: List[Int], inter: Int): Int = l match {
+            case Nil => inter
+            case h :: t => lenCal(t, inter + 1)
+        }
+        lenCal(l, 0)
+    }
+
+    // Exercise 3.4
+    def append(l: List[Int], n: Int): List[Int] = l match {
+        case Nil => List(n)
+        case h :: t => h :: append(t, n)
+    }
 }
